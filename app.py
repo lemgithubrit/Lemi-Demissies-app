d578bb09e61990daf5f16ea29ae3ad0fb204c843
lemgithubrit <108199053+lemgithubrit@users.noreply.github.com>
2022-07-03 13:17:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -146,19 +146,29 @@ st.markdown("<h1 style='text-align: center; color: gray;'>Streamlit Is a Faster 
 
 
 
-import streamlit as st
-from bokeh.plotting import figure
+import pandas as pd
+import numpy as np
+import altair as alt
+df = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+c = alt.Chart(df).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.altair_chart(c, use_container_width=True)
 
-x = [1, 2, 3, 4, 5]
-y = [6, 7, 2, 4, 5]
+import pandas as pd
+import numpy as np
+import altair as alt
+chart_data = pd.DataFrame(
+     np.random.randn(50, 3),
+     columns=["a", "b", "c"])
+import pandas as pd
+import numpy as np
 
-p = figure(
-     title='simple line example',
-     x_axis_label='x',
-     y_axis_label='y')
-
-p.line(x, y, legend_label='Trend', line_width=2)
-
-st.bokeh_chart(p, use_container_width=True)
+st.bar_chart(chart_data)
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+st.line_chart(chart_data)
 
 
