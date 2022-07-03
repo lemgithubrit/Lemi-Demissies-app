cc185a3e7e4dec7627701ccf013334d93ad7332c
lemgithubrit <108199053+lemgithubrit@users.noreply.github.com>
2022-07-03 12:09:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -142,6 +142,25 @@ st.markdown("<h1 style='text-align: center; color: gray;'>Streamlit Is a Faster 
 
 
 
+import pandas as pd
+import streamlit as st 
+
+
+
+df = pd.read_csv("data/titanic.csv")
+
+def highlight_survived(s):
+    return ['background-color: green']*len(s) if s.Survived else ['background-color: red']*len(s)
+
+def color_survived(val):
+    color = 'green' if val else 'red'
+    return f'background-color: {color}'
+
+st.dataframe(df.style.apply(highlight_survived, axis=1))
+st.dataframe(df.style.applymap(color_survived, subset=['Survived']))
+ 
+
+
 
 
 
