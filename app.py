0b4663dff8e0e308b185a9ea0a0d456878a9a6c3
lemgithubrit <108199053+lemgithubrit@users.noreply.github.com>
2022-07-03 08:26:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -144,9 +144,5 @@ st.markdown("<h1 style='text-align: center; color: gray;'>Streamlit Is a Faster 
 
 
 
-st.time_input('Set an alarm for', datetime.time(8, 45))
-st.write('Alarm is set for', t)
-
-
 
 
