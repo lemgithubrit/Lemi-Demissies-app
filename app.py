ef28bc562acc16063bc4cda7d015264160d4c16c
lemgithubrit <108199053+lemgithubrit@users.noreply.github.com>
2022-07-06 18:43:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -86,6 +86,27 @@ st.download_button(
      mime='text/csv',
  )
 
+#line graph
+import streamlit as st 
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+fig = make_subplots(rows=1, cols=2)
+#First SubPlot
+fig.add_trace(
+    go.Scatter(
+        x=[1, 2, 3], 
+        y=[4, 5, 6]),
+        row=1, col=1
+    )
+#Second SubPlot
+fig.add_trace(
+    go.Scatter(
+        x=[20, 30, 40], 
+        y=[50, 60, 70]),
+        row=1, col=2
+    )
+
+st.plotly_chart(fig)
 
 #chart representation
 import pandas as pd
