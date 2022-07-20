2a65d982b6fdd0ebe9f5a560888cbf4747c8293b
lemgithubrit <108199053+lemgithubrit@users.noreply.github.com>
2022-07-20 23:07:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -149,29 +149,6 @@ st.markdown("<h1 style='text-align: center; color: gray;'>Streamlit Is a Faster 
 
 
 
-import streamlit as st
-import pandas as pd
-import plotly.graph_objects as go
-
-#Data Set
-countries=['India', 'Australia',
-           'Japan', 'America',
-           'Russia']
- 
-values = [4500, 2500, 1053, 500,
-          3200]
-
-#The plot
-fig = go.Figure(
-    go.Pie(
-    labels = countries,
-    values = values,
-    hoverinfo = "label+percent",
-    textinfo = "value"
-))
-
-st.header("Pie chart")
-st.plotly_chart(fig)
 
 
 
